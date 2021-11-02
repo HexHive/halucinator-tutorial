5183bcb462d0f93b5b07b3d1372c97ce0ff37a6a
Antony Vennard <antony@vennard.ch>
2021-11-02 16:25:55 +0100

    Start pulling everything together.

---
+++ genreadmes.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+pushd 01-firststeps
+pandoc README.md > README.html
+popd
+
+pushd 02-interact
+pandoc README.md > README.html
+popd
+
+pushd 03-bugs
+pandoc README.md > README.html
+popd
+
+pandoc README.md > README.html
+
