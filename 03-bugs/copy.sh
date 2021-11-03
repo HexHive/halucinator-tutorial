e025c2995f4e3ad54355394fe33bac395ebe3cd7
Mathias Payer <mathias.payer@nebelwelt.net>
2021-11-03 16:40:50 +0100

    Permissions

--- 03-bugs/copy.sh
+++ 03-bugs/copy.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-cp -fv ../02-firststeps/config.yaml .
-cp -fv ../02-firststeps/mem.yaml .
-cp -fv ../02-firststeps/addrs.yaml .
-cp -fv ../02-firststeps/run.yaml .
-cp -fv ../02-firststeps/stm32-f411re-uart.bin .
-cp -fv ../02-firststeps/stm32-f411re-uart.elf .
+cp -fv ../01-firststeps/config.yaml .
+cp -fv ../01-firststeps/mem.yaml .
+cp -fv ../01-firststeps/addrs.yaml .
+cp -fv ../01-firststeps/run.sh .
+cp -fv ../01-firststeps/stm32-f411re-uart.bin .
+cp -fv ../01-firststeps/stm32-f411re-uart.elf .
+cp -fv ../02-firststeps/extdev .
